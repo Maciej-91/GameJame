c6e5f67195211a900bc64ad9b4af032df453f899
Simon Bruyelle <simon@printycolors.fr>
2023-04-11 20:58:04 +0200

    feat(): spaceship and planets added

--- src/client/script/index.js
+++ src/client/script/index.js
@@ -7,23 +7,64 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 800 }
+            gravity: { y: 0 }
         }
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
-    this.load.image('spaceship', './static/img/spaceship.png');
+let spaceship;
+let cursors;
+let obstacles = [];
+
+function randomPosition(){
+    const x = 1920;
+    const y = Math.floor(Math.random() * 1080);
+    return {x, y};
 }
 
-function create ()
-{
-    this.physics.add.image(200, 200, 'spaceship');
+function preload (){
+    this.load.image('spaceship', './static/img/millennium-falcon.png');
+    this.load.image('obstacle', './static/img/death-star.png');
+}
+
+function create (){
+    spaceship = this.physics.add.image(500, 1, 'spaceship');
+    spaceship.setScale(0.5);
+    spaceship.setCollideWorldBounds(true);
+    spaceship.body.gravity.y = 300;
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    for (let i = 0; i < 3; i++) createObstacle(this);
+
+    this.physics.world.on('worldbounds', (body) => body.gameObject && body.gameObject.type === 'obstacle' && removeObstacle(body.gameObject));
+
+    setInterval(() => createObstacle(this) , 2000);
+}
+
+function update (){
+    if(cursors.space.isDown) spaceship.setVelocity(0, -200);
+
+    if(this.physics.world.overlap(spaceship, obstacles)) this.scene.restart();
+}
+
+function createObstacle(scene){
+    let obstacle = scene.physics.add.image(randomPosition().x, randomPosition().y, 'obstacle');
+    obstacle.setScale(0.5);
+    obstacle.setImmovable(true);
+    obstacle.setVelocityX(-300);
+    obstacle.type = 'obstacle';
+    obstacles.push(obstacle);
+}
+
+function removeObstacle(obstacle){
+    obstacles.splice(obstacles.indexOf(obstacle), 1);
+    obstacle.destroy();
 }
