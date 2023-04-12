1729edbb3717590bd47eeff0f290190cd367e250
Simon Bruyelle <simon@printycolors.fr>
2023-04-12 14:49:59 +0200

    feat(level): level creation

--- src/client/script/index.js
+++ src/client/script/index.js
@@ -1,9 +1,22 @@
 import Phaser from 'phaser';
+import Levels from './levels.js';
+
+function getLevel(level) {
+    if(!Levels(getScreenSize().height)[level]) throw new Error('Level not found');
+    return Levels(getScreenSize().height)[level];
+}
+
+function getScreenSize() {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+}
 
 const config = {
     type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
+    width: getScreenSize().width,
+    height: getScreenSize().height,
     physics: {
         default: 'arcade',
         arcade: {
@@ -22,12 +35,9 @@ var game = new Phaser.Game(config);
 let spaceship;
 let cursors;
 let planets = [];
-
-function randomPosition(){
-    const x = 1920;
-    const y = Math.floor(Math.random() * 1080);
-    return {x, y};
-}
+let frameIndex = 0;
+let frameIntervalId;
+let currentLevel = getLevel(0);
 
 function preload (){
     this.load.image('spaceship', './static/img/millennium-falcon.png');
@@ -35,36 +45,59 @@ function preload (){
 }
 
 function create (){
-    spaceship = this.physics.add.image(500, 1, 'spaceship');
-    spaceship.setScale(0.5);
+    spaceship = this.physics.add.image(getScreenSize().width * 0.2, getScreenSize().height / 2, 'spaceship');
+    spaceship.setScale(0.3);
     spaceship.setCollideWorldBounds(true);
     spaceship.body.gravity.y = 300;
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    for (let i = 0; i < 3; i++) createplanet(this);
+    this.physics.world.on('worldbounds', (body) => body.gameObject && body.gameObject.type === 'planet' && removePlanet(body.gameObject));
 
-    this.physics.world.on('worldbounds', (body) => body.gameObject && body.gameObject.type === 'planet' && removeplanet(body.gameObject));
-
-    setInterval(() => createplanet(this) , 2000);
+    frameIntervalId = setInterval(() => createPlanets(this, currentLevel.frames[frameIndex]), 1000);
 }
 
 function update (){
     if(cursors.space.isDown) spaceship.setVelocity(0, -200);
 
-    if(this.physics.world.overlap(spaceship, planets)) this.scene.restart();
+    if(this.physics.world.overlap(spaceship, planets)) {
+        clearInterval(frameIntervalId);
+        frameIndex = 0;
+        removePlanets();
+        this.scene.pause();
+    }
+
+    const lastPlanet = planets[planets.length - 1];
+    if(lastPlanet && lastPlanet.x + lastPlanet.width / 2 < 0) {
+        clearInterval(frameIntervalId);
+        frameIndex = 0;
+        removePlanets();
+        this.scene.pause();
+    };
 }
 
-function createplanet(scene){
-    let planet = scene.physics.add.image(randomPosition().x, randomPosition().y, 'planet');
-    planet.setScale(0.5);
+function createPlanet(game, planetData){
+    let planet = game.physics.add.image((getScreenSize().width + 100 + planetData.x || 0), planetData.y, 'planet');
+    planet.setScale(0.3);
     planet.setImmovable(true);
     planet.setVelocityX(-300);
     planet.type = 'planet';
     planets.push(planet);
 }
 
-function removeplanet(planet){
+function createPlanets(game, frame){
+    if(!frame) return;
+    frame.forEach((planetData) => setTimeout(() => createPlanet(game, planetData), planetData));
+    frameIndex++;
+    if (frameIndex >= currentLevel.frames.length - 1) clearInterval(frameIntervalId);
+}
+
+function removePlanet(planet){
     planets.splice(planets.indexOf(planet), 1);
     planet.destroy();
+}
+
+function removePlanets(){
+    planets.forEach(planet => planet.destroy());
+    planets = [];
 }
---
+++ src/client/script/levels.js
@@ -0,0 +1,75 @@
+const Levels = (positionY) => [
+    {
+        "name": "Niveau 1",
+        "frames": [
+          [
+            { "x": 0, "y": positionY * 0.1 },
+            { "x": 50, "y": positionY * 0.3 }
+          ],
+          [
+            { "x": 50, "y": positionY * 0.9 }
+          ],
+          [
+            { "x": 50, "y": positionY * 0.4 },
+            { "x": 300, "y": positionY * 0.5 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.9 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.3 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.7 },
+            { "x": -100, "y": positionY * 0.9 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.5 },
+            { "x": 100, "y": positionY * 0.9 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.2 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.8 },
+            { "x": -150, "y": positionY * 0.5 }
+          ],
+          [
+            { "x": 100, "y": positionY * 0.3 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.5 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.1 },
+            { "x": -200, "y": positionY * 0.3 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.9 },
+            { "x": 150, "y": positionY * 0.3 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.5 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.7 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.3 },
+            { "x": 200, "y": positionY * 0.1 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.9 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.5 }
+          ],
+          [
+            { "x": 0, "y": positionY * 0.3 },
+            { "x": -50, "y": positionY * 0.1 }
+          ]
+        ]
+    }
+];
+
+export default Levels;
