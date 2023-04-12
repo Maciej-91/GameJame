c724d5b3e5024cafbad4bdbb0e73d1be8b865dac
lseeuws <louis.seeuws@outlook.com>
2023-04-12 11:08:40 +0200

    Bandeau

--- src/client/script/index.js
+++ src/client/script/index.js
@@ -51,15 +51,16 @@ function preload (){
 function create (){
 
     //-----------------------bandeau-------------------------
-    this.add.rectangle(0, 0, config.width, 70, 0xff0000).setOrigin(0, 0);
+    this.add.rectangle(0, 0, config.width, 70, 0xff0000).setOrigin(0, 0).setDepth(1);;
 
-    let niveau_bandeau = this.add.text(50, 18, `niveau : ${niveau} `, style_bandeau);
+    let niveau_bandeau = this.add.text(50, 18, `niveau : ${niveau} `, style_bandeau).setDepth(1);
     niveau_bandeau.setOrigin(0, 0);
-    let score_bandeau = this.add.text((config.width / 2) - 130, 18, `Score : ${score} `, style_bandeau);
+    let score_bandeau = this.add.text((config.width / 2) - 130, 18, `Votre scores : ${score} `, style_bandeau).setDepth(1);
     score_bandeau.setOrigin(0, 0);
 
     for (let i = 0; i < vie; i++) {
         coeur.push(this.add.image(config.width - (i * 75) - 80, 38, 'heart'));
+        coeur[i].setDepth(1);
     }
     
     spaceship = this.physics.add.image(500, 1, 'spaceship');
