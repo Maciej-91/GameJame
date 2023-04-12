f7fea8dc43e3b2b34d46d1c683d515782947e5d7
Maciej-91 <maciek.2205.mk@gmail.com>
2023-04-12 16:11:45 +0200

    clean code

--- src/client/script/index.js
+++ src/client/script/index.js
@@ -53,41 +53,16 @@ function create (){
         this.scene.pause();
         const closeModal = document.getElementById('game-over-modal');
         closeModal.classList.remove('hidden');
-        // const modal = gameOverModal();
-        // document.body.appendChild(modal);
-        // modal.classList.remove('hidden');
-        // console.log(modal)
-
-        // document.getElementById('game-over-restart').addEventListener('click', () =>  startG(modal, this.scene))
-        // document.getElementById('game-over-restart').addEventListener('click', () =>  startG(modal, this.scene))
 
         const restartGame = () => {
-          console.log("restartGame")
           closeModal.classList.add('hidden');
           this.scene.restart();
           document.getElementById('game-over-restart').removeEventListener('click', restartGame);
-          console.log("test")
       }
-      // console.log(document.getElementById('game-over-restart'))
-  
       document.getElementById('game-over-restart').addEventListener('click', restartGame);
 
 
     });
-
-        //document.getElementById('game-over-restart').removeEventListener('click', () => this.scene.restart());
-    //     document.getElementById('game-over-restart').addEventListener('click', () => {
-    //         console.log("eventListener")
-    //         modal.classList.add('hidden');
-    //         this.scene.restart();
-    //     });
-    
-    //     document.getElementById('game-over-exit').addEventListener('click', () => {
-    //         modal.classList.add('hidden');
-    //         this.scene.stop();
-    //     }, {once: true});
-    //   });
-    
 }
 
 function update (){
@@ -108,47 +83,4 @@ function createplanet(scene){
 function removeplanet(planet){
     planets.splice(planets.indexOf(planet), 1);
     planet.destroy();
-}
-
-function gameOverModal() {
-    const modal = document.createElement('div');
-    modal.className = 'fixed z-10 inset-0 overflow-y-auto hidden';
-    // modal.id = 'game-over-modal';
-    modal.innerHTML = `
-  <div id='game-over-modal' class="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-    <div class="fixed inset-0 transition-opacity">
-      <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-    </div>
-    <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <div class="sm:flex sm:items-start">
-          <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-            <!-- L'icône de la modal -->
-            <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
-            </svg>
-          </div>
-          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-            <h3 class="text-lg leading-6 font-medium text-gray-900">
-              Game Over
-            </h3>
-            <div class="mt-2">
-              <p class="text-sm text-gray-500">
-                Vous avez perdu ! Essayez encore ?
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-        <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm" id="game-over-restart">
-          Recommencer
-        </button>
-        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" id="game-over-exit">
-          Quitter
-        </button>
-      </div>
-    </div>`
-    return modal
 }
